d03a5eefedc80d941f6028166662d15b98c500e1
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-01-29 16:07:15 +0200

    Add files via upload

---
+++ Reverse Integer.cs
@@ -0,0 +1,21 @@
+﻿int x = 120;
+int reverse = 0;
+bool flag = true;
+if(x<0)
+{
+    flag = false;
+    x = -x;
+}
+while(x>0)
+{
+    int digit = x % 10;
+    x = x / 10;
+    reverse = reverse + digit;
+    reverse = reverse * 10;
+}
+reverse = reverse / 10;
+if(flag==false)
+{
+    reverse = -reverse;
+}
+Console.WriteLine(reverse);
