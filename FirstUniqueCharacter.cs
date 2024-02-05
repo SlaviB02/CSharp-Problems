dcaef8503b76f3aab8c5125c988fe80995e13ced
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-05 12:21:41 +0200

    Add files via upload

---
+++ FirstUniqueCharacter.cs
@@ -0,0 +1,26 @@
+﻿var s = "abracadabra";
+Dictionary<char,int>letters= new Dictionary<char,int>();
+for (int i = 0; i < s.Length; i++)
+{
+    if (letters.ContainsKey(s[i]))
+    {
+        letters[s[i]]++;
+    }
+    else
+    {
+        letters.Add(s[i], 1);
+    }
+
+}
+var orderedLetters = letters.OrderBy(x => x.Value);
+var first = orderedLetters.First();
+if(first.Value!=1)
+{
+    Console.WriteLine("-1");
+}
+else
+{
+    Console.WriteLine(s.IndexOf(first.Key));
+}
+
+    
