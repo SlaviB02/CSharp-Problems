ececf4f1f2077e7632833e81a3d47adc055ba081
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-05 14:37:57 +0200

    Add files via upload

---
+++ TwoSum.cs
@@ -0,0 +1,24 @@
+﻿var nums = new int[] { 3,3 };
+int target = 6;
+int firstIndex = 0;
+int secondIndex = 0;
+bool flag = true;
+for (int i = 0; i < nums.Length; i++)
+{
+    for(int j = i+1;j<nums.Length; j++)
+    {
+        var sum = nums[j] + nums[i];
+        if(sum==target)
+        {
+            firstIndex = i;
+            secondIndex = j;
+            flag = false;
+            break;
+        }
+    }
+    if(!flag)
+    {
+        break;
+    }
+}
+Console.WriteLine(firstIndex + "  " + secondIndex);
