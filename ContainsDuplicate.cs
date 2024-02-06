2fe23e2abd90bf2774d5f6e589a6dde0752f9543
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-06 23:32:37 +0200

    Add files via upload

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,17 @@
+﻿bool ContainsDuplicate(int[] nums)
+{
+    Dictionary<int,int> result = new Dictionary<int,int>();
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if (result.ContainsKey(nums[i]))
+        {
+            return true;
+        }
+        else
+        {
+            result.Add(nums[i],1);
+        }
+    }
+    return false;
+}
+Console.WriteLine(ContainsDuplicate(new int[] { 1, 2, 3 }));
