0e0b917034e411d75709fd91803424b829fc8052
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-06 11:43:19 +0200

    Add files via upload

---
+++ GroupAnagrams.cs
@@ -0,0 +1,38 @@
+﻿var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+IList<IList<string>> result = new List<IList<string>>();
+var sortedStrings=new List<string>();
+
+for (int i = 0; i < strs.Length; i++)
+{
+    char[] temp = strs[i].ToArray();
+    Array.Sort(temp);
+    var str = new string(temp);
+    sortedStrings.Add(str);
+  
+}
+var wordCount=new Dictionary<string, List<int>>();
+for(int i = 0;i < sortedStrings.Count;i++)
+{
+    
+    if (wordCount.ContainsKey(sortedStrings[i]))
+    {
+        wordCount[sortedStrings[i]].Add(i);
+    }
+    else
+    {
+        wordCount.Add(sortedStrings[i], new List<int>() {});
+        wordCount[sortedStrings[i]].Add(i);
+    }
+
+}
+foreach(var word in wordCount)
+{
+    List<string>list=new List<string>();
+    for(int i = 0;i<word.Value.Count;i++)
+    {
+        list.Add(strs[word.Value[i]]);
+    }
+    result.Add(list);
+    
+}
+return result;
