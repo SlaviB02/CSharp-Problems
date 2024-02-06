112d98964306d4b72cd15d603462149c1b6c3bcc
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-06 21:01:27 +0200

    Add files via upload

---
+++ SingleNumber.cs
@@ -0,0 +1,19 @@
+﻿int SingleNumber(int[] nums)
+{
+    Dictionary<int,int>numbers= new Dictionary<int,int>();
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if (numbers.ContainsKey(nums[i])) 
+        {
+            numbers[nums[i]]++;
+        }
+        else
+        {
+            numbers.Add(nums[i], 1);
+        }
+    }
+
+    return numbers.First(x => x.Value == 1).Key;
+}
+var num=SingleNumber(new int[] { 2, 2, 1 });
+Console.WriteLine(num);
