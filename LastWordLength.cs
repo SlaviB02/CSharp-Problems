f1909b3005c1a326fb3928a64d4c8436e2edbd04
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-06 16:12:13 +0200

    Add files via upload

---
+++ LastWordLength.cs
@@ -0,0 +1,8 @@
+﻿int LengthOfLastWord(string s)
+{
+    var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    var lastWord = words[words.Length-1];
+    return lastWord.Length;
+}
+string word = "Hello World";
+Console.WriteLine(LengthOfLastWord(word));
