f2aef5aaf395229a88da799d939cd806f83e301a
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-07 08:22:06 +0200

    Add files via upload

---
+++ SortCharactersByFrequency.cs
@@ -0,0 +1,26 @@
+﻿string FrequencySort(string s)
+{
+    Dictionary<char,int>letters= new Dictionary<char,int>();
+    for(int i = 0; i < s.Length; i++)
+    {
+        if (letters.ContainsKey(s[i]))
+        {
+            letters[s[i]]++;
+        }
+        else
+        {
+            letters.Add(s[i], 1);
+        }
+    }
+    string result = "";
+    foreach(var let in letters.OrderByDescending(x=>x.Value))
+    {
+        for(int i = 0;i<let.Value;i++)
+        {
+            result += let.Key;
+        }
+    }
+    return result;
+}
+string s = "Aabb";
+Console.WriteLine(FrequencySort(s));
