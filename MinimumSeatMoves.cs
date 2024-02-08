439e4530e5a599015bf25a946ebcd6520eeee3cb
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-08 10:56:25 +0200

    Add files via upload

---
+++ MinimumSeatMoves.cs
@@ -0,0 +1,26 @@
+﻿int MinMovesToSeat(int[] seats, int[] students)
+{
+    int count = 0;
+    Array.Sort(seats);
+    Array.Sort(students);
+      for(int i=0;i<seats.Length;i++)
+    {
+        int move = 0;
+        if (seats[i] > students[i])
+        {
+            move = 1;
+        }
+        else
+        {
+            move = -1;
+        }
+        while (seats[i] != students[i]) 
+        {
+            students[i]+=move;
+            count++;
+        }
+    }
+    return count;
+}
+int a = MinMovesToSeat(new int[] { 3, 1, 5 }, new int[] { 2, 7, 4 });
+Console.WriteLine(a);
