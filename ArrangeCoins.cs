7e4422230fc38c6f889f3d050e7e69666315cd05
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-08 10:35:38 +0200

    Add files via upload

---
+++ ArrangeCoins.cs
@@ -0,0 +1,17 @@
+﻿int ArrangeCoins(int n)
+{
+    int count = 0;
+    int row = 1;
+    while(n>0)
+    {
+        n -= row;
+       if(n>=0)
+        {
+            count++;
+        }
+        row++;
+    }
+    return count;
+}
+var a = ArrangeCoins(6);
+Console.WriteLine(a);
