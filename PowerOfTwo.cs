af045614c1d82bdf2480a4e69e8097f098dd2f63
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-08 14:32:25 +0200

    Add files via upload

---
+++ PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿bool IsPowerOfTwo(int n)
+{
+    if (n == 0) return false;
+    if(n>0)
+    {
+        for (int i = 0; i < 31; i++)
+        {
+            double pow = Math.Pow(2, i);
+            if (pow == n)
+            {
+                return true;
+            }
+        }
+    }
+   
+
+
+    return false;
+}
+
+var a = IsPowerOfTwo(-4);
+Console.WriteLine(a);
