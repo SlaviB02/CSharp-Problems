eda4cac334c0bd9fd97b7126a152ad3ef7bccf5d
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-10 11:57:52 +0200

    Add files via upload

---
+++ PalindromicSubstrings.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text;
+int CountSubstrings(string s)
+{
+    int count = 0;
+    for (int i = 0; i < s.Length; ++i)
+    {
+        StringBuilder subString = new StringBuilder(s.Length - i);
+        for (int j = i; j < s.Length; ++j)
+        {
+            subString.Append(s[j]);
+            if(checkPalindrome(subString.ToString()))
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+bool checkPalindrome(string s)
+{
+    char[]letters=s.ToCharArray();
+    Array.Reverse(letters);
+    string reversed=new string(letters);
+    if(s==reversed)
+    {
+        return true;
+    }
+    return false;
+}
+string s = "aaa";
+
+var a = CountSubstrings(s);
+Console.WriteLine(a);
