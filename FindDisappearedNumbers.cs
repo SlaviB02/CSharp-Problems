49151ecf38d337910e54f92e11656489baec39d4
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-10 17:10:53 +0200

    Add files via upload

---
+++ FindDisappearedNumbers.cs
@@ -0,0 +1,16 @@
+﻿IList<int> FindDisappearedNumbers(int[] nums)
+{
+    IList<int> result = new List<int>();
+       for(int i = 1; i <=nums.Length; i++)
+        {
+       if(!nums.Contains(i))
+        {
+            result.Add(i);
+        }
+        }
+    return result;
+    
+}
+int[] nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+var list = FindDisappearedNumbers(nums);
+Console.WriteLine(String.Join(",", list));
---
+++ FoundValuesBy2.cs
@@ -0,0 +1,18 @@
+﻿int FindFinalValue(int[] nums, int original)
+{
+    while(true)
+    {
+        if(nums.Contains(original))
+        {
+            original *= 2;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return original;
+}
+int[] nums = new int[] { 5, 3, 6, 1, 12 };
+int original = 3;
+Console.WriteLine(FindFinalValue(nums, original));
