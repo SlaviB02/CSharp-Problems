2e1071c7adbeba437643b732ad8aa6245d72621d
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-11 20:47:17 +0200

    Add files via upload

---
+++ SelfDividingNumbers.cs
@@ -0,0 +1,26 @@
+﻿IList<int> SelfDividingNumbers(int left, int right)
+{
+    IList<int> result = new List<int>();
+    for(int i = left; i <= right;i++)
+    {
+        int temp = i;
+        bool flag = true;
+        while(temp>0)
+        {
+            int digit=temp%10;
+            if(digit==0 || i % digit != 0)
+            {
+                flag = false;
+                break;
+            }
+            temp /= 10;
+        }
+        if(flag)
+        {
+            result.Add(i);
+        }
+    }
+    return result;
+}
+var list = SelfDividingNumbers(47, 85);
+Console.WriteLine(String.Join(",",list));
