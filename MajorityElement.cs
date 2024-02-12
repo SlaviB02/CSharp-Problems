03cf990c7029f5ef485ef03f14fbd96f14074a3c
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-12 16:03:05 +0200

    Add files via upload

---
+++ MajorityElement.cs
@@ -0,0 +1,16 @@
+﻿int MajorityElement(int[] nums)
+{
+    Dictionary<int,int> result = new Dictionary<int,int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (result.ContainsKey(nums[i]))
+        {
+            result[nums[i]]++;
+        }
+        else
+        {
+            result.Add(nums[i], 1);
+        }
+    }
+    return result.OrderByDescending(x => x.Value).First().Key;
+}
