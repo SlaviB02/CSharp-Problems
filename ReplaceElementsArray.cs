69a71cfce1507638f297ff7fc38b612042b1048f
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-13 12:01:19 +0200

    Add files via upload

---
+++ ReplaceElementsArray.cs
@@ -0,0 +1,11 @@
+﻿int[] ArrayChange(int[] nums, int[][] operations)
+{
+    for(int i = 0; i<operations.Length;i++)
+    {
+        int number= operations[i][0];
+        int replacement= operations[i][1];
+        int index = Array.IndexOf(nums, number);
+        nums[index] = replacement;
+    }
+    return nums;
+}
