3fccbeb4b8682f81ff68fa13fda885aaf920ecec
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-14 08:50:24 +0200

    Add files via upload

---
+++ ArrayElementsBySign.cs
@@ -0,0 +1,30 @@
+﻿int[] RearrangeArray(int[] nums)
+{
+    Queue<int> negative = new Queue<int>();
+    Queue<int>positive = new Queue<int>();
+    foreach(int n in nums)
+    {
+        if(n > 0)
+        {
+            positive.Enqueue(n);
+        }
+        else
+        {
+            negative.Enqueue(n);
+        }
+    }
+    int[]result=new int[nums.Length];
+    for(int i = 0;i<nums.Length;i++)
+    {
+        if(i%2==0)
+        {
+            result[i] = positive.Dequeue();
+        }
+        else
+        {
+            result[i]=negative.Dequeue();
+        }
+    }
+    return result;
+}
+int[] nums = new int[] { 3, 1, -2, -5, 2, -4 };
