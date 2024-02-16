e205be5d90964feef30203732366029cee68cb76
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-16 19:25:56 +0200

    Add files via upload

---
+++ UniqueNumberOfOccurrences.cs
@@ -0,0 +1,21 @@
+﻿bool UniqueOccurrences(int[] arr)
+{
+    Dictionary<int,int> result = new Dictionary<int,int>(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (result.ContainsKey(arr[i]))
+        {
+            result[arr[i]]++;
+        }
+        else {
+
+            result.Add(arr[i], 1);
+        }
+    }
+   var temp= result.DistinctBy(x=>x.Value);
+     if(temp.Count()!=result.Count())
+    {
+        return false;
+    }
+    return true;
+}
