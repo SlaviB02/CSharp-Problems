8fdca4da1b9155d7b95f7a4a45ebaf8f806bd217
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-17 23:04:51 +0200

    Add files via upload

---
+++ IntersectionOfTwoArrays.cs
@@ -0,0 +1,15 @@
+﻿int[] Intersection(int[] nums1, int[] nums2)
+{
+    HashSet<int>result=new HashSet<int>();
+    for (int i = 0; i < nums1.Length; i++)
+    {
+        foreach(int n in nums2)
+        {
+            if (nums1[i]==n)
+            {
+                result.Add(nums1[i]);
+            }
+        }
+    }
+    return result.ToArray();
+}
