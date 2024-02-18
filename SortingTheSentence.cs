485cae62f4684e86a905d7adfa01e2d77bda46ad
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-18 20:35:38 +0200

    Add files via upload

---
+++ SortingTheSentence.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+string SortSentence(string s)
+{
+    string[]words=s.Split(" ");
+    string[] ordered = new string[words.Length];
+    foreach (string word in words)
+    {
+        int order = word[word.Length - 1]-'0';
+        ordered[order - 1] = word.Substring(0,word.Length-1);
+    }
+    StringBuilder sb = new StringBuilder();
+    foreach (string word in ordered)
+    {
+        sb.Append(word+" ");
+    }
+   
+    return sb.ToString().TrimEnd();
+}
+string str = "is2 sentence4 This1 a3";
+Console.WriteLine(SortSentence(str));
