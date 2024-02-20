ffea5bbc829191189f3ae2bc221ef757db90dd68
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-20 12:50:14 +0200

    Add files via upload

---
+++ MissingNumber.cs
@@ -0,0 +1,13 @@
+﻿int MissingNumber(int[] nums)
+{
+    for (int i = 0; i <= nums.Length; i++)
+    {
+        if (!nums.Contains(i))
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+int a = MissingNumber(new int[] { 3, 0, 1 });
+Console.WriteLine(a);
