107a15efacb81cc7a99cd3ca6fe174b6f7830cb5
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-21 08:42:39 +0200

    Add files via upload

---
+++ ProductAndSumOfDigits.cs
@@ -0,0 +1,15 @@
+﻿int SubtractProductAndSum(int n)
+{
+    int sum = 0;
+    int mult = 1;
+    while(n>0)
+    {
+        int digit=n%10;
+        sum += digit;
+        mult *= digit;
+        n /= 10;
+    }
+    return mult - sum;
+}
+int n = 114;
+Console.WriteLine(SubtractProductAndSum(n));
