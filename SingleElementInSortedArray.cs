78b14b92757ca3aa6d93315a2cdb9ad8998790ed
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-23 09:49:54 +0200

    Add files via upload

---
+++ SingleElementInSortedArray.cs
@@ -0,0 +1,25 @@
+﻿int SingleNonDuplicate(int[] nums)
+{
+    if(nums.Length==1)
+    {
+        return nums[0];
+    }
+    for(int i=0;i<nums.Length-1;i++)
+    {
+        if (nums[i] == nums[i+1])
+        {
+            i++;
+        }
+        else
+        {
+            return nums[i];
+        }
+        if(i==nums.Length-2)
+        {
+            return nums[i+1];
+        }
+    }
+    return -1;
+}
+int[] nums = new int[] { 1,1,2};
+Console.WriteLine(SingleNonDuplicate(nums));
