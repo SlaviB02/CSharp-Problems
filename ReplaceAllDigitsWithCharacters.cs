93de1c8219b494992ed44827eac95bb2764c92ed
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-24 17:52:27 +0200

    Add files via upload

---
+++ ReplaceAllDigitsWithCharacters.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string ReplaceDigits(string s)
+{
+    StringBuilder sb = new StringBuilder();
+    for(int i=0;i<s.Length-1;i+=2)
+    {
+       
+     
+        char c= s[i];
+        char x = s[i + 1];
+        int num = x - '0';
+        sb.Append(s[i]);
+        sb.Append((char)((int)c+num));
+        
+        
+    }
+    if (!char.IsDigit(s[s.Length - 1]))
+        sb.Append(s[s.Length - 1]);
+    return sb.ToString();
+
+}
+string s = "a1c1e1";
+Console.WriteLine(ReplaceDigits(s));
