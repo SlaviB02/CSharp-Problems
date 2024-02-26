76a70f7285f834e4486bcfd996eca85214b78d4a
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-26 18:13:21 +0200

    Add files via upload

---
+++ ReverseWordsInAString.cs
@@ -0,0 +1,15 @@
+﻿string ReverseWords(string s)
+{
+    char[]letters=s.ToCharArray();
+    Array.Reverse(letters);
+    string reversed=new string(letters);
+    string[]words=reversed.Split(' ');
+    string result = "";
+    for(int i=words.Length-1; i>=0; i--)
+    {
+        result += words[i]+" ";
+    }
+    return result.TrimEnd();
+}
+string s = "Let's take LeetCode contest";
+Console.WriteLine(ReverseWords(s));
