23619725809d31a025162d8e0145736bf607e78c
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-27 13:35:33 +0200

    Add files via upload

---
+++ CalculateMoneyInBank.cs
@@ -0,0 +1,21 @@
+﻿int TotalMoney(int n)
+{
+    int mondayMoney = 1;
+    int total = 0;
+    int currentMoney = 1;
+    for(int i=1;i<=n;i++)
+    {
+        total += currentMoney;
+        currentMoney++;
+        if (i%7==0)
+        {
+            mondayMoney += 1;
+            currentMoney = mondayMoney;
+        }
+        
+       
+    }
+    return total;
+}
+var res = TotalMoney(10);
+Console.WriteLine(res);
