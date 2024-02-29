b19758098d4f168b6c0e2b069de5e208b4eea604
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-02-29 15:43:28 +0200

    Add files via upload

---
+++ FindSpecial_Integer.cs
@@ -0,0 +1,18 @@
+﻿int FindSpecialInteger(int[] arr)
+{
+    Dictionary<int,int>res=new Dictionary<int,int>();
+    foreach(int num in arr)
+    {
+        if(res.ContainsKey(num))
+        {
+            res[num]++;
+        }
+        else
+        {
+            res.Add(num, 1);
+        }
+    }
+    return res.OrderByDescending(x=>x.Value).First().Key;
+}
+int[] arr = new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 };
+Console.WriteLine(FindSpecialInteger(arr));
