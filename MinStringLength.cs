86d8bb6b7e40c3749c1f46bf1c0475b65ab41d84
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-01 12:44:05 +0200

    Add files via upload

---
+++ MinStringLength.cs
@@ -0,0 +1,23 @@
+﻿int MinLength(string s)
+{
+    while(true)
+    {
+        if(s.Contains("AB"))
+        {
+            int index=s.IndexOf("AB");
+            s=s.Remove(index,2);
+        }
+        else if(s.Contains("CD"))
+        {
+            int index = s.IndexOf("CD");
+            s=s.Remove(index, 2);
+        }
+        else
+        {
+            break;
+        }
+    }
+    return s.Length;
+}
+string s = "ABFCACDB";
+Console.WriteLine(MinLength(s));
