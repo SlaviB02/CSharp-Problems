75a02166f2c2ae0a05b470efd050a0f47f5305cb
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-03 02:11:21 +0200

    Add files via upload

---
+++ CheckXMatrix.cs
@@ -0,0 +1,16 @@
+﻿bool CheckXMatrix(int[][] grid)
+{
+    for(int i=0; i<grid.Length; i++)
+    {
+        for(int j=0; j < grid[i].Length; j++)
+        {
+           if(i==j || (i + j) == grid.Length - 1)
+            {
+                if (grid[i][j] == 0) { return false; }
+            }
+
+            else if (grid[i][j] !=0) { return false; }
+        }
+    }
+    return true;
+}
