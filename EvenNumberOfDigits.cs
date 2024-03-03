7f8a046323259113875395c77a644de234202cf8
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-04 01:16:42 +0200

    Add files via upload

---
+++ EvenNumberOfDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+int FindNumbers(int[] nums)
+{
+    int total = 0;
+   for(int i=0; i<nums.Length; i++)
+    {
+        int countDigit = 0;
+        while (nums[i] > 0)
+        {
+            nums[i] = nums[i] / 10;
+            countDigit++;
+        }
+        if(countDigit%2== 0)
+        {
+            total++;
+        }
+    }
+    return total;
+}
