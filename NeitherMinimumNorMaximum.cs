21fe692418fad3808cb93825cdc6260ac6444a31
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-06 00:11:07 +0200

    Add files via upload

---
+++ NeitherMinimumNorMaximum.cs
@@ -0,0 +1,13 @@
+﻿int FindNonMinOrMax(int[] nums)
+{
+    var list=nums.ToList();
+    var max=list.Max();
+    var min=list.Min();
+    list.Remove(max);
+    list.Remove(min);
+    if(list.Count > 0)
+    {
+        return list[0];
+    }
+    return -1;
+}
