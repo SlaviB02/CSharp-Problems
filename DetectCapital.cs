ea8370c2fab3421e7e1a11a057e127e851d00969
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-08 00:41:22 +0200

    Add files via upload

---
+++ DetectCapital.cs
@@ -0,0 +1,19 @@
+﻿bool DetectCapitalUse(string word)
+{
+    string toUpper=word.ToUpper();
+    if(word==toUpper) return true;
+    string toLower=word.ToLower();
+    if(word==toLower) return true;
+    string thenLower = word.Substring(1).ToLower();
+    if (Char.IsUpper(word[0]))
+    {
+        if(thenLower==word.Substring(1))
+        {
+            Console.WriteLine(thenLower);
+            return true;
+        }
+    }
+    return false;
+}
+var res = DetectCapitalUse("FlaG");
+Console.WriteLine(res);
