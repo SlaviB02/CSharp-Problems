061d2d33643c6499a57d7deb4290644e2c864df2
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-09 00:01:38 +0200

    Add files via upload

---
+++ ElementsWithMaxFrequency.cs
@@ -0,0 +1,17 @@
+﻿int MaxFrequencyElements(int[] nums)
+{
+    Dictionary<int,int>result= new Dictionary<int,int>();
+    foreach(int el in nums)
+    {
+        if(result.ContainsKey(el))
+        {
+            result[el]++;
+        }
+        else
+        {
+            result.Add(el, 1 );
+        }
+    }
+    int maxFrequency=result.Max(x=>x.Value);
+    return result.Where(x=>x.Value==maxFrequency).Sum(a=>a.Value);
+}
---
+++ SetMismatch.cs
@@ -0,0 +1,37 @@
+﻿int[] FindErrorNums(int[] nums)
+{
+    int[] result = new int[2];
+    int start = 1;
+    int end = nums.Length;
+    int doubledElement = 0;
+    int missingElement = 0;
+    Console.WriteLine(end);
+    Dictionary<int,int>doubled= new Dictionary<int,int>();
+    for(int i=0;i<nums.Length;i++)
+    {
+        if (doubled.ContainsKey(nums[i]))
+        {
+            doubledElement = nums[i];
+            break;
+        }
+        else
+        {
+            doubled.Add(nums[i], 1);
+        }
+    }
+    for(int i = start; i <= end; i++)
+    {
+        
+        if (!nums.Contains(i))
+        {
+            missingElement = i;
+        }
+    }
+    result[0]= doubledElement;
+    result[1]= missingElement;
+    return result;
+
+}
+int[]arr= new int[] { 1, 2, 2, 4 };
+var result=FindErrorNums(arr);
+Console.WriteLine(String.Join(",", result));
