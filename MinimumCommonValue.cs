45fc01cb1317306f4790ab57834ffe64781061db
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-10 00:27:34 +0200

    Add files via upload

---
+++ MinimumCommonValue.cs
@@ -0,0 +1,13 @@
+﻿int GetCommon(int[] nums1, int[] nums2)
+{
+    for (int i = 0; i < nums1.Length; i++)
+    {
+        if (nums2.Contains(nums1[i]))
+        {
+            return nums1[i];
+        }
+    }
+    return -1;
+}
+int res = GetCommon(new int[] { 1, 2, 3 }, new int[] { 2, 4 });
+Console.WriteLine(res);
