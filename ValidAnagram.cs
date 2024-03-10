0f86d2ca33d688faa37610b4f318d90740e0b582
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-11 00:45:33 +0200

    Add files via upload

---
+++ ValidAnagram.cs
@@ -0,0 +1,14 @@
+﻿bool IsAnagram(string s, string t)
+{
+    char[]sLetters= s.ToCharArray();
+    Array.Sort(sLetters);
+    char[]tLetters= t.ToCharArray();
+    Array.Sort(tLetters);
+    if(new string(sLetters)==new string(tLetters))
+    {
+        return true;
+    }
+    return false;
+}
+string s = "anagram", t = "nagaram";
+Console.WriteLine(IsAnagram(s,t));
