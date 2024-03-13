f360d7ae68ab5598fbec7f244c9c27e42e21a073
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-14 00:37:25 +0200

    Add files via upload

---
+++ PivotInteger.cs
@@ -0,0 +1,21 @@
+﻿int PivotInteger(int n)
+{
+    int sumX = 0;
+    for (int i = 1; i <=n; i++)
+    {
+        int sumN = 0;
+        sumX += i;
+        for (int j = i; j <=n; j++)
+        {
+            sumN += j;
+        }
+      
+        if(sumX==sumN)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int n = 8;
+Console.WriteLine(PivotInteger(n));
