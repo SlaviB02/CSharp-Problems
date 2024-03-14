faf86c1091faf76d59fd9e4f5c2844ea27f23b3b
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-15 00:03:40 +0200

    Add files via upload

---
+++ BinarySubarraysWithSum.cs
@@ -0,0 +1,32 @@
+﻿int NumSubarraysWithSum(int[] nums, int goal)
+{
+    int count = 0;
+  
+    for(int i = 0; i < nums.Length; i++)
+    {
+        int sum = 0;
+        if (nums[i] ==goal)
+        {
+            count++;
+        }
+        sum += nums[i];
+        for (int j=i+1; j<nums.Length; j++)
+        {
+            
+            sum += nums[j];
+
+            if (sum == goal)
+            {
+                count++;
+                
+            }
+        }
+        
+
+    }
+   return count;
+}
+int[] nums = new int[] { 0, 0, 0, 0, 0 };
+int goal = 0;
+int result = NumSubarraysWithSum(nums, goal);
+Console.WriteLine(result);  
