9cd83c6aacf3a092ec4994603e4fb33c6883732e
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-18 00:02:26 +0200

    Add files via upload

---
+++ SubstringsStartingAndEndingWithCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+long CountSubstrings(string s, char c)
+{
+    long count = 0;
+    for (int i = 0; i < s.Length; ++i)
+    {
+        StringBuilder sb = new StringBuilder(s.Length - i);
+        for (int j = i; j < s.Length; ++j)
+        {
+            sb.Append(s[j]);
+            if (sb[0]==c && sb[sb.Length-1]==c)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+string s = "abada";
+char c = 'a';
+var result = CountSubstrings(s, c);
+Console.WriteLine(result);
