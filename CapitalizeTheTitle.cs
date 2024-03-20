349e2703d03bfbf27c736cc8c509c0630388bd0f
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-21 00:04:28 +0200

    Add files via upload

---
+++ CapitalizeTheTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+string CapitalizeTitle(string title)
+{
+    string[]words=title.Split(' ');
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <=2)
+        {
+            sb.Append(words[i].ToLower()+" ");
+        }
+       else
+        {
+            string word = words[i].Substring(0,1);
+            word = word.ToUpper();
+            word += words[i].Substring(1).ToLower();
+            sb.Append(word+" ");
+        }
+    }
+    return sb.ToString().TrimEnd();
+}
+string title = "capiTalIze tHe titLe";
+string result=CapitalizeTitle(title);
+Console.WriteLine(result);
