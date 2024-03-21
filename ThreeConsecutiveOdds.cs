2ef976eb0bf87af43fd1cb7ca2c00cb48864948b
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-22 00:01:34 +0200

    Add files via upload

---
+++ ThreeConsecutiveOdds.cs
@@ -0,0 +1,15 @@
+﻿bool ThreeConsecutiveOdds(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        if(i+1==arr.Length || i+2==arr.Length)
+        {
+            break;
+        }
+        if (arr[i] %2!=0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
