ce3288ae85fa5f64134d02197b32c235b1f4fd3c
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-23 01:45:01 +0200

    Add files via upload

---
+++ ValidPalindrome.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+bool IsPalindrome(string s)
+{
+    s= s.ToLower();
+    Regex rgx = new Regex("[^a-zA-Z0-9]");
+    s = rgx.Replace(s, "");
+    string res=new string(s.ToCharArray()
+         .Where(c => !Char.IsWhiteSpace(c))
+         .ToArray());
+    string start=res.Substring(0, res.Length/2);
+    char[] letters = res.ToCharArray();
+    Array.Reverse(letters);
+    string temp=new string(letters);
+    string end=temp.Substring(0, temp.Length/2);
+    Console.WriteLine(start);
+    Console.WriteLine(end);
+    if (start==end)
+    {
+        return true;
+    }
+    return false;
+}
+string s = "A man, a plan, a canal -- Panama";
+Console.WriteLine(IsPalindrome(s));
