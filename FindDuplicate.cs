d85ea56c2253c8f217cceb4f51b518f305df56bf
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-25 00:02:06 +0200

    Add files via upload

---
+++ FindDuplicate.cs
@@ -0,0 +1,16 @@
+﻿int FindDuplicate(int[] nums)
+{
+    List<int>numbers=new List<int>();
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if (!numbers.Contains(nums[i]))
+        {
+            numbers.Add(nums[i]);
+        }
+        else
+        {
+            return nums[i];
+        }
+    }
+    return 0;
+}
