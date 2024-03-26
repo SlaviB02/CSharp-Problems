b0a7c8d3778627561ecd3eadc45bf28551ddc822
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-27 00:03:11 +0200

    Add files via upload

---
+++ FirstMissingPositive.cs
@@ -0,0 +1,11 @@
+﻿int FirstMissingPositive(int[] nums)
+{
+    for(int i=1;i<int.MaxValue;i++)
+    {
+        if(!nums.Contains(i))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
