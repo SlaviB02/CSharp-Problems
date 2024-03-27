10d8209f4bc6618f86f171815d1b9e9060b9659b
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-28 00:02:10 +0200

    Add files via upload

---
+++ FindAllDuplicatesInArray.cs
@@ -0,0 +1,17 @@
+﻿IList<int> FindDuplicates(int[] nums)
+{
+    Dictionary<int,int>numbers=new Dictionary<int,int>(); 
+    List<int>result=new List<int>();
+    for(int i=0; i<nums.Length; i++)
+    {
+        if (!numbers.ContainsKey(nums[i]))
+        {
+            numbers.Add(nums[i],1);
+        }
+        else
+        {
+            result.Add(nums[i]);
+        }
+    }
+    return result;
+}
