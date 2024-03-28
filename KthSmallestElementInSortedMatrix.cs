469e5cd4940e28ea769f1341ee24bbcb1dd811a3
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-29 00:09:10 +0200

    Add files via upload

---
+++ KthSmallestElementInSortedMatrix.cs
@@ -0,0 +1,16 @@
+﻿int KthSmallest(int[][] matrix, int k)
+{
+    List<int> result = new List<int>();
+    for(int i = 0; i < matrix.Length; i++)
+    {
+        for(int j=0; j < matrix[i].Length; j++)
+        {
+            result.Add(matrix[i][j]);
+        }
+    }
+    var sorted=result.OrderBy(x => x).ToList();
+    return sorted[k - 1];
+}
+int[][] matrix = [[1, 5, 9], [10, 11, 13], [12, 13, 15]];
+int k = 8;
+Console.WriteLine(KthSmallest(matrix,k));
