fd12bc326d59d7105f5251caf17de8c1d9750790
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-03-30 00:36:12 +0200

    Add files via upload

---
+++ MinimumNumberGame.cs
@@ -0,0 +1,21 @@
+﻿int[] NumberGame(int[] nums)
+{
+    int[]arr = new int[nums.Length];
+    int index = 0;
+    var list=nums.ToList().OrderBy(x=>x).ToList();
+    while(list.Count>0)
+    {
+        int num1 = list[0];
+        int num2 = list[1];
+        Console.WriteLine(index);
+        arr[index]=num2;
+        index++;
+        arr[index]=num1;
+        index++;
+        list.RemoveRange(0, 2);
+    }
+    return arr;
+}
+int[] arr = new int[] { 5, 4, 2, 3 };
+var result=NumberGame(arr);
+Console.WriteLine(String.Join(",", result));
