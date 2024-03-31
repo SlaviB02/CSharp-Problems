fde56446e559f509b26181937ded5e76b0a906df
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-04-01 00:01:19 +0300

    Add files via upload

---
+++ SecondLargestDigitInString.cs
@@ -0,0 +1,22 @@
+﻿int SecondHighest(string s)
+{
+    List<int> list = new List<int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (Char.IsDigit(s[i]))
+        {
+            list.Add(s[i]-'0');
+           
+        }
+    }
+    var nums = list.Distinct().OrderByDescending(x => x).ToList();
+    if (nums.Count<2)
+    {
+        return -1;
+    }
+   
+    return nums[1];
+}
+string s = "dfa12321afd";
+int result=SecondHighest(s);
+Console.WriteLine(result);
