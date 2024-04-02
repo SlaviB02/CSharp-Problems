701630a9d6b497ddb8cc626163df657604c43a12
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-04-03 00:27:03 +0300

    Add files via upload

---
+++ RichestCustomerWealth.cs
@@ -0,0 +1,20 @@
+﻿int MaximumWealth(int[][] accounts)
+{
+    int maxWealth = 0;
+    for(int i= 0; i < accounts.Length; i++)
+    {
+        int sum = 0;
+        for(int j= 0; j < accounts[i].Length; j++)
+        {
+            sum+= accounts[i][j];
+        }
+        if(maxWealth < sum)
+        {
+            maxWealth = sum;
+        }
+    }
+    return maxWealth;
+}
+int[][] accounts = [[1, 2, 3], [3, 2, 1]];
+int result=MaximumWealth(accounts);
+Console.WriteLine(result);
