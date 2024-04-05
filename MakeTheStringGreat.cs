70aab01e89304b6d5497a8b0158c6de24f59924d
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-04-06 00:22:16 +0300

    Add files via upload

---
+++ MakeTheStringGreat.cs
@@ -0,0 +1,33 @@
+﻿string MakeGood(string s)
+{
+    int index = 0;
+    while(true)
+    { 
+        if(index+1>=s.Length)
+        {
+            break;
+        }
+        char letter1 = s[index];
+        char letter2 = s[index+1];
+         if (char.IsUpper(letter1) && char.IsLower(letter2) || char.IsUpper(letter2) && char.IsLower(letter1))
+        {
+            if (char.ToLower(letter1) == char.ToLower(letter2))
+            {
+              s=s.Remove(index,2);
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
+         else
+        {
+            index++;
+        }
+    }
+    return s;
+}
+string s = "abBAcC";
+var result=MakeGood(s);
+Console.WriteLine(result);
