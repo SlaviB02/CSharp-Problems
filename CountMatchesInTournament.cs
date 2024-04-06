97e2d59784286f11e8985b31cacf27f91ea8a9d4
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-04-07 00:20:16 +0300

    Add files via upload

---
+++ CountMatchesInTournament.cs
@@ -0,0 +1,31 @@
+﻿
+int NumberOfMatches(int n)
+{
+    int count = 0;
+    if(n%2==0)
+    {
+        int matches = n / 2;
+        count = matches;
+        while (matches>1) 
+        {
+            int advance = matches / 2;
+            matches-=advance;
+            count+=advance;
+        }
+    }
+    else
+    {
+        int matches = (n - 1) / 2;
+        count = matches;
+        while (matches > 0)
+        {
+            int advance = (matches-1) / 2 +1;
+            matches -= advance;
+            count += advance;
+        }
+    }
+    return count;
+}
+int n = 7;
+int result=NumberOfMatches(n);
+Console.WriteLine(result);
