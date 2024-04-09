71996a9a67b11b197e771eac9c947b9b2483062f
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-09 17:22:39 +0300

    Create FindTheDifference.cs

---
+++ FindTheDifference.cs
@@ -0,0 +1,17 @@
+﻿char FindTheDifference(string s, string t)
+{
+    List<char>letters = new List<char>();
+   letters=s.ToCharArray().ToList();
+    
+    for(int i=0; i<letters.Count; i++)
+    {
+        int index = t.IndexOf(letters[i]);
+       t=t.Remove(index,1);
+       
+    }
+    return t[0];
+   
+}
+string s = "abcd", t = "abcde";
+char res=FindTheDifference(s, t);
+Console.WriteLine(res);
