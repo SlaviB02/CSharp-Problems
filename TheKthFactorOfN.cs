53f443f000faad8728e297586760ecd58f4a6abc
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-11 11:41:27 +0300

    New Task

---
+++ TheKthFactorOfN.cs
@@ -0,0 +1,16 @@
+﻿int KthFactor(int n, int k)
+{
+    List<int>factors= new List<int>();
+    for(int i = 1; i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+            factors.Add(i);
+        }
+    }
+    if(k>factors.Count())
+    {
+        return -1;
+    }
+    return factors[k-1];
+}
