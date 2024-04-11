46ae69416265ce7fbf783b650d1bf91bc7329e72
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-12 00:14:47 +0300

    Add Task

---
+++ ContainsDuplicateII.cs
@@ -0,0 +1,21 @@
+﻿bool ContainsNearbyDuplicate(int[] nums, int k)
+{
+    for(int i = 0; i < nums.Length; i++)
+    {
+        for(int j = i+1;j<nums.Length;j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                if(Math.Abs(i-j)<=k)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+int[] nums = [1, 2, 3, 1];
+int k = 3;
+bool result=ContainsNearbyDuplicate(nums, k);
+Console.WriteLine(result);
