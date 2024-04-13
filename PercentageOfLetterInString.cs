819249f3d3f3c73c501d6c67bad219c451dd8a71
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-14 00:19:12 +0300

    Create PercentageOfLetterInString.cs

---
+++ PercentageOfLetterInString.cs
@@ -0,0 +1,17 @@
+﻿int PercentageLetter(string s, char letter)
+{
+    int times = 0;
+    for(int i = 0; i < s.Length; i++)
+    { 
+        if (s[i] == letter) 
+        { 
+            times++;
+        }    
+    }
+    double result = (times / (double)s.Length)*100;
+    return (int)result;
+}
+string s = "foobar";
+char letter = 'o';
+int res = PercentageLetter(s, letter);
+Console.WriteLine(res);
