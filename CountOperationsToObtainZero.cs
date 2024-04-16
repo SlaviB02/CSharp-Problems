edbc66c9b993b3ca7d28e2eb0e59166c7f863aa0
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-16 16:59:44 +0300

    Add Task

---
+++ CountOperationsToObtainZero.cs
@@ -0,0 +1,25 @@
+﻿int CountOperations(int num1, int num2)
+{
+    int count = 0;
+    while(num1!=0 && num2!=0)
+    {
+        if(num1>num2)
+        {
+            num1-=num2;
+        }
+        else if(num1<num2)
+        {
+            num2-=num1;
+        }
+        else
+        {
+            num1 = 0;
+            num2= 0;
+        }
+        count++;
+    }
+    return count;
+}
+int num1 = 2, num2 = 3;
+int result=CountOperations(num1, num2);
+Console.WriteLine(result);
