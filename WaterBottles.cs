08906693698792804c0e907a3621e9c879740833
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-23 00:01:59 +0300

    Add Task

---
+++ WaterBottles.cs
@@ -0,0 +1,21 @@
+﻿int NumWaterBottles(int numBottles, int numExchange)
+{
+    int count = numBottles;
+    while(true)
+    {
+      
+        if (numBottles<numExchange)
+        {
+            break;
+        }
+        int remainder=numBottles%numExchange;
+        numBottles /= numExchange;
+        count += numBottles;
+        numBottles=remainder+numBottles;
+
+    }
+    return count;
+}
+int bottles = 15, exchange = 8;
+int res=NumWaterBottles(bottles, exchange);
+Console.WriteLine(res);
