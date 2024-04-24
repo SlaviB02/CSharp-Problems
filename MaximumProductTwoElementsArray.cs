ef2c27090c4332351078ea03ebf0bb48347aa590
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-25 00:08:23 +0300

    AddTask

---
+++ MaximumProductTwoElementsArray.cs
@@ -0,0 +1,12 @@
+﻿int MaxProduct(int[] nums)
+{
+    List<int>numbers=nums.ToList();
+    int max1 = numbers.Max();
+    int index=numbers.IndexOf(max1);
+    numbers.RemoveAt(index);
+    int max2=numbers.Max();
+    return (max1-1)*(max2-1);
+}
+int[] nums = [3, 4, 5, 2];
+int max=MaxProduct(nums);
+Console.WriteLine(max);
