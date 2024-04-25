07ee42a70937d7e9d67d38debc061439d717b9e0
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-26 00:08:18 +0300

    Add Task

---
+++ MaxWordsInSentence.cs
@@ -0,0 +1,16 @@
+﻿int MostWordsFound(string[] sentences)
+{
+    int maxCount = 0;
+    for(int i = 0; i < sentences.Length; i++)
+    {
+        string[] words = sentences[i].Split(" ");
+        if (words.Length > maxCount)
+        {
+            maxCount = words.Length;
+        }
+    }
+    return maxCount;
+}
+string[] sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"];
+int words=MostWordsFound(sentences);
+Console.WriteLine(words);
