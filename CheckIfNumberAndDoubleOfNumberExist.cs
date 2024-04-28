5f1d803ec130f6628aecb6991b4233e6ed09b81a
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-29 00:06:15 +0300

    Add Task

---
+++ CheckIfNumberAndDoubleOfNumberExist.cs
@@ -0,0 +1,16 @@
+﻿bool CheckIfExist(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        int first = arr[i];
+        for(int j=i+1;j<arr.Length;j++)
+        {
+            int second = arr[j];
+            if(first==2*second || second==2*first)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
