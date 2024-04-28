951a2a1fb444cfe93d9f1f1082ec4ec28e819d18
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-28 10:14:29 +0300

    Add Task

---
+++ SequentialDigits.cs
@@ -0,0 +1,46 @@
+﻿IList<int> SequentialDigits(int low, int high)
+{
+    List<int> result = new List<int>();
+    int left=low;
+    int right = high;
+    while(true)
+    {
+
+        if(left==right || left>right)
+        {
+            break;
+        }
+        if(checkDigits(left))
+        {
+            result.Add(left);
+        }
+        if (checkDigits(right))
+        {
+            result.Add(right);
+        }
+        left++;
+        right--;
+        
+    }
+    result.Sort();
+    return result;
+}
+bool checkDigits(int n)
+{
+    int current = n % 10;
+    n/=10;
+    while(n>0)
+    {
+        int digit = n % 10;
+        if(current-1!=digit)
+        {
+            return false;
+        }
+        current=digit; 
+        n /= 10;
+    }
+    return true;
+}
+int low = 58, high = 155;
+var res=SequentialDigits(low, high);
+Console.WriteLine(String.Join(",", res));
