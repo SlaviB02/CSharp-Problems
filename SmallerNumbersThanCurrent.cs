d77d03d833bda43aad7cdf534bb63354512af66f
Slavi Bozhilov <slavib2002@abv.bg>
2024-04-30 13:45:50 +0300

    Add Task

---
+++ SmallerNumbersThanCurrent.cs
@@ -0,0 +1,27 @@
+﻿int[] SmallerNumbersThanCurrent(int[] nums)
+{
+    int index = 0;
+    int[]result= new int[nums.Length];
+    while(true)
+    {
+        if(index>=nums.Length)
+        {
+            break;
+        }
+        int current= nums[index];
+        int count = 0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            if (nums[index] >nums[i])
+            {
+                count++;
+            }
+        }
+        result[index] = count;
+        index++;
+    }
+    return result;
+}
+int[] nums = [8, 1, 2, 2, 3];
+int[]res=SmallerNumbersThanCurrent(nums);
+Console.WriteLine (String.Join (", ",res));
