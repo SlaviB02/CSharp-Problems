704fa5881adb3aed095c741c44c1673859c68067
Slavi Bozhilov <slavib2002@abv.bg>
2024-05-05 12:18:59 +0300

    Add Task

---
+++ AddDigits.cs
@@ -0,0 +1,23 @@
+﻿int AddDigits(int num)
+{
+    while(true)
+    {
+        if(num<10)
+        {
+            return num;
+        }
+        int temp = num;
+        int sum = 0;
+        while(temp>0)
+        {
+            int digit = temp%10;
+            sum+= digit;
+            temp /=10;
+        }
+        num = sum;
+    }
+
+}
+int n = 38;
+int res=AddDigits(n);
+Console.WriteLine(res);
