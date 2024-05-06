7cdfe3247c8c93758a234e1fdff4b5cc44406c5b
Slavi Bozhilov <slavib2002@abv.bg>
2024-05-06 10:03:36 +0300

    Add Task

---
+++ FaultyKeyboard.cs
@@ -0,0 +1,22 @@
+﻿string FinalString(string s)
+{
+    string res = "";
+    for(int i= 0; i < s.Length; i++)
+    {
+        char letter= s[i];
+        if(letter=='i')
+        {
+            char[]arr=res.ToCharArray();
+            Array.Reverse(arr);
+            res=new string(arr);
+        }
+        else
+        {
+            res+=letter;
+        }
+    }
+    return res;
+}
+string s = "string";
+string res=FinalString(s);
+Console.WriteLine(res);
