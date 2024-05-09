3159cbf5b82d5c644ea784208f221b278700d723
Slavi Bozhilov <slavib2002@abv.bg>
2024-05-09 10:26:06 +0300

    Add Task

---
+++ XorOperationArray.cs
@@ -0,0 +1,17 @@
+﻿int XorOperation(int n, int start)
+{
+    int[]arr=new int[n];
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = start + 2 * i;
+    }
+    int result = 0;
+    for(int i=0; i<n; ++i)
+    {
+        result ^= arr[i];
+    }
+    return result;
+}
+int n = 5, start = 0;
+int res=XorOperation(n, start);
+Console.WriteLine(res);
