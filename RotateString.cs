5e76dcd36c4e8c3585804eef45bcba517794a122
Slavi Bozhilov <slavib2002@abv.bg>
2024-05-15 16:48:06 +0300

    Add Task

---
+++ RotateString.cs
@@ -0,0 +1,23 @@
+﻿bool RotateString(string s, string goal)
+{
+    int count = 0;
+    while(true)
+    {
+        if(s==goal)
+        {
+            return true;
+        }
+        if(count==s.Length)
+        {
+            return false;
+        }
+        char last = s[s.Length-1];
+        string temp=s.Substring(0, s.Length-1);
+        string res = last + temp;
+        s = res;
+        count++;
+    }
+}
+string s = "abcde", goal = "cdeab";
+bool res=RotateString(s, goal);
+Console.WriteLine(res);
