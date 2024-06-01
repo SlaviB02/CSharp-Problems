fb65f0b1321bc24b3facc6eaf4666d54749721f9
Slavi Bozhilov <slavib2002@abv.bg>
2024-06-02 00:17:38 +0300

    Add Task

---
+++ ScoreOfString.cs
@@ -0,0 +1,13 @@
+﻿int ScoreOfString(string s)
+{
+    int sum = 0;
+    for(int i=0;i<s.Length-1;i++)
+    {
+        int diff = Math.Abs(s[i]-s[i+1]);
+        sum += diff;
+    }
+    return sum;
+}
+string s = "hello";
+int score=ScoreOfString(s);
+Console.WriteLine(score);
