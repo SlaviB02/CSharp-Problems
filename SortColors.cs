e69a7f740c231653b584fdaf351d0a4e2aa91f60
Slavi Bozhilov <slavib2002@abv.bg>
2024-06-12 10:48:17 +0300

    Add Task

---
+++ SortColors.cs
@@ -0,0 +1,39 @@
+﻿void SortColors(int[] nums)
+{
+    nums =SortArray(nums,0,nums.Length-1);
+}
+
+int[] SortArray(int[] array, int leftIndex, int rightIndex)
+{
+    var i = leftIndex;
+    var j = rightIndex;
+    var pivot = array[leftIndex];
+    while (i <= j)
+    {
+        while (array[i] < pivot)
+        {
+            i++;
+        }
+
+        while (array[j] > pivot)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    if (leftIndex < j)
+        SortArray(array, leftIndex, j);
+    if (i < rightIndex)
+        SortArray(array, i, rightIndex);
+    return array;
+}
+int []nums = [2, 0, 2, 1, 1, 0];
+SortColors(nums);
