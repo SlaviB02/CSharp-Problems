892f22853adc5bed686f5c8afbb14c6f9518bcf5
Slavi Bozhilov <slavib2002@abv.bg>
2024-07-06 10:31:53 +0300

    Add Task

---
+++ PassThePillow.cs
@@ -0,0 +1,28 @@
+﻿int PassThePillow(int n, int time)
+{
+    int index = 1;
+    int count = 1;
+    int countPassedTimes = 0;
+    while(true)
+    {
+        if(count==n)
+        {
+            index = -1;
+        }
+        if (count == 1)
+        {
+            index = 1;
+        }
+        if(countPassedTimes==time)
+        {
+            break;
+        }
+        count += index;
+        countPassedTimes++;
+    }
+    return count;
+}
+
+int n = 4, time = 5;
+int res=PassThePillow(n,time);
+Console.WriteLine(res);
