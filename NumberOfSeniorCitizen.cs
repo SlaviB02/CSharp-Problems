6809fe7dbdd83d2cabad4193ae13f9f970b7ad20
Slavi Bozhilov <slavib2002@abv.bg>
2024-08-01 11:43:57 +0300

    Add Task

---
+++ NumberOfSeniorCitizen.cs
@@ -0,0 +1,18 @@
+﻿int CountSeniors(string[] details)
+{
+    int count = 0;
+    foreach (var d in details)
+    {
+        string ageString=d.Substring(11,2);
+        int age = int.Parse(ageString);
+        if(age>60)
+        {
+            count++;
+        }
+        
+    }
+    return count;
+}
+string[] details = ["7868190130M7522", "5303914400F9211", "9273338290F4010"];
+int res = CountSeniors(details);
+Console.WriteLine(res);
