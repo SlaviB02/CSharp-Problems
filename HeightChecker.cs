a221c6e309a1ae1e992d42ebcf608317c6d4b16b
Slavi Bozhilov <slavib2002@abv.bg>
2024-08-07 19:58:16 +0300

    AddTask

---
+++ HeightChecker.cs
@@ -0,0 +1,24 @@
+﻿int HeightChecker(int[] heights)
+{
+    int[]expected=heights.ToArray();
+
+    Array.Sort(expected);
+
+
+    int count = 0;
+
+    for(int i= 0; i < expected.Length; i++)
+    {
+        if(expected[i] != heights[i])
+        {
+            count++;
+        }
+    }
+
+
+    return count;
+}
+
+int[] heights = [1, 1, 4, 2, 1, 3];
+int res=HeightChecker(heights);
+Console.WriteLine(res);
