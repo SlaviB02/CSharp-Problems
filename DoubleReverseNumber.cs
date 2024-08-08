89379174597223bf80b56bd9dd57ce11d8be4b03
Slavi Bozhilov <slavib2002@abv.bg>
2024-08-08 10:07:14 +0300

    AddTask

---
+++ DoubleReverseNumber.cs
@@ -0,0 +1,40 @@
+﻿bool IsSameAfterReversals(int num)
+{
+    int reversedNum = 0;
+    int temp = num;
+    
+
+    while (temp > 0)
+    {
+        reversedNum += temp % 10;
+        reversedNum *= 10;
+        temp /= 10;
+
+    }
+
+    reversedNum /= 10;
+    
+
+    int doubleReversed = 0;
+
+    while(reversedNum>0)
+    {
+        doubleReversed += reversedNum % 10;
+        doubleReversed *= 10;
+        reversedNum /= 10;
+    }
+
+    doubleReversed /= 10;
+
+    
+
+    if(doubleReversed==num)
+    {
+        return true;
+    }
+
+    return false;
+}
+int num = 526;
+bool res=IsSameAfterReversals(num);
+Console.WriteLine(res);
