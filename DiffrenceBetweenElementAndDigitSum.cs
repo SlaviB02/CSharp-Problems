c9654b88606cd0243870fd5358ada2709ae84f4d
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-09 19:02:57 +0300

    Add files via upload

---
+++ DiffrenceBetweenElementAndDigitSum.cs
@@ -0,0 +1,25 @@
+﻿int DifferenceOfSum(int[] nums)
+{
+    int ElementSum=nums.Sum();
+
+    int DigitSum = 0;
+
+    string numbers = "";
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        numbers += nums[i];
+    }
+
+    for(int i=0;i<numbers.Length;i++)
+    {
+        DigitSum +=int.Parse(numbers[i].ToString());
+    }
+
+    return Math.Abs(ElementSum-DigitSum);
+}
+
+int[] nums = [1, 15, 6, 3];
+int res=DifferenceOfSum(nums);
+
+Console.WriteLine(res);
