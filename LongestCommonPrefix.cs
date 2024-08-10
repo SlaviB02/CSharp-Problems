7ff8544485e67c871b00a5f10c1486d69c8da5cb
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-10 09:52:05 +0300

    Add files via upload

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,34 @@
+﻿string LongestCommonPrefix(string[] strs)
+{
+    string prefix = "";
+
+    List<string> list =strs.OrderBy(x=>x.Length).ToList();
+    string word = list[0];
+    for(int i=0; i < word.Length; i++)
+    {
+        prefix += word[i];
+        bool flag = true;
+        for(int j=1;j<list.Count;j++)
+        {
+            string prf =list[j].Substring(0,prefix.Length);
+
+            if(prf!=prefix)
+            {
+                flag= false;
+                prefix=prefix.Substring(0,prefix.Length-1);
+                break;
+            }
+            
+        }
+        if(!flag)
+        {
+            break;
+        }
+    }
+    return prefix;
+}
+string[] strs = ["reflower", "flow", "flight"];
+
+string res=LongestCommonPrefix(strs);
+
+Console.WriteLine(res);
