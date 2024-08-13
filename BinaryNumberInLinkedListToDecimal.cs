49cb3698562fe93e0e8fd0ad48331734040eca13
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-14 00:07:54 +0300

    Add files via upload

---
+++ BinaryNumberInLinkedListToDecimal.cs
@@ -0,0 +1,25 @@
+﻿
+
+int GetDecimalValue(ListNode head)
+{
+    string bitNumber = "";
+    while(head != null)
+    {
+        bitNumber += head.val;
+        head = head.next;
+    }
+
+    int res=Convert.ToInt32(bitNumber,2);
+
+    return res;
+}
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
