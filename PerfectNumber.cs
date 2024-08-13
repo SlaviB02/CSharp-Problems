0c4ac8b51c8adeb073d370a3ede6a474b331b5d8
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-13 09:57:07 +0300

    Add files via upload

---
+++ PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿bool CheckPerfectNumber(int num)
+{
+    int sum = 1;
+
+    if(num==1)
+    {
+        return true;
+    }
+
+    for(int i=2;i<=num/2;i++)
+    {
+        if(num%i==0)
+        {
+            sum += i;
+        }
+
+
+        if(sum>num)
+        {
+            return false;
+        }
+    }
+
+    if(sum==num)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int num = 28;
+
+var res=CheckPerfectNumber(num);
+
+Console.WriteLine(res);
