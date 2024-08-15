151071fffd97380e93c16ecb023f1205dde02d0a
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-15 11:29:26 +0300

    Add files via upload

---
+++ LemonadeChange.cs
@@ -0,0 +1,66 @@
+﻿bool LemonadeChange(int[] bills)
+{
+    Dictionary<int,int>money= new Dictionary<int,int>();
+
+    money.Add(5,0);
+    money.Add(10,0);
+    money.Add(20,0);
+
+    for (int i = 0; i < bills.Length; i++)
+    {
+        money[bills[i]]++;
+            
+        if(bills[i]==10)
+        {
+            if (money[5]==0)
+            {
+                return false;
+            }
+            else
+            {
+                money[5]--;
+            }
+        }
+
+        if (bills[i]==20)
+        {
+            if (money[5] == 0 && money[10] == 0)
+            {
+                return false;
+            }
+            else if (money[5] == 0)
+            {
+                return false;
+            }
+            else if (money[10]==0)
+            {
+                money[5]-=3;
+            }
+            else
+            {
+                money[5]--;
+                money[10]--;
+            }
+        }
+
+        if(money[5]<0)
+        {
+            return false;
+        }
+        //foreach(var k in money)
+        //{
+        //    Console.WriteLine($"{k.Key}->{k.Value}");
+        //}
+        //Console.WriteLine("----");
+
+    }
+
+    return true;
+
+}
+
+int[] bills = [5, 5, 5, 10, 5, 5, 10, 20, 20, 20];
+
+var res=LemonadeChange(bills);
+
+Console.WriteLine(res);
