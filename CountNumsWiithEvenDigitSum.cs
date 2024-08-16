e7192fd15b6408b13f2a33f93c3068d3833786f2
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-16 16:33:16 +0300

    Add files via upload

---
+++ CountNumsWiithEvenDigitSum.cs
@@ -0,0 +1,24 @@
+﻿int CountEven(int num)
+{
+    int count = 0;
+    for(int i=2;i<=num;i++)
+    {
+        int temp = i;
+        int sum = 0;
+        while(temp>0)
+        {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if(sum%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int num = 30;
+
+int res=CountEven(num);
+
+Console.WriteLine(res);
