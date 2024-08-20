9e7f350386e2acfeaade664b9330f45aeb82ded8
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-20 16:24:41 +0300

    Add files via upload

---
+++ StringMatchingInArray.cs
@@ -0,0 +1,31 @@
+﻿IList<string> StringMatching(string[] words)
+{
+
+    List<string> result = new List<string>();
+
+    for(int i=0;i< words.Length;i++)
+    {
+        string word=words[i];
+        for(int j=0;j< words.Length;j++)
+        {
+            if(j==i)
+            {
+                continue;
+            }
+
+            if (words[j].Contains(word))
+            {
+                result.Add(word);
+            }
+
+        }
+    }
+
+    return result.Distinct().ToList();
+}
+
+string[] words = ["mass", "as", "hero", "superhero"];
+
+var res=StringMatching(words);
+
+Console.WriteLine(string.Join(",",res));
