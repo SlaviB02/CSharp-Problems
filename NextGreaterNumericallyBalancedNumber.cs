c826e2770e4b5781feef4d4f3e3c280cc14e4b00
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-21 09:37:19 +0300

    Add files via upload

---
+++ NextGreaterNumericallyBalancedNumber.cs
@@ -0,0 +1,49 @@
+﻿int NextBeautifulNumber(int n)
+{
+    while(true)
+    {
+        n++;
+        if(isBalanced(n))
+        {
+            return n;
+        }
+    }
+}
+bool isBalanced(int n)
+{
+    Dictionary<int, int> digits = new Dictionary<int, int>();
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        if (digit == 0)
+        {
+            return false;
+        }
+
+        if (digits.ContainsKey(digit))
+        {
+            digits[digit]++;
+        }
+        else
+        {
+            digits.Add(digit, 1);
+        }
+        n /= 10;
+    }
+
+    foreach (var kvp in digits)
+    {
+        if(kvp.Key !=kvp.Value)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int n = 1000;
+
+int res = NextBeautifulNumber(n);
+
+Console.WriteLine(res);
