e8adce16166a89d46e76df8d9d90718ba27aaef9
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-22 01:04:30 +0300

    Add files via upload

---
+++ AppleRedistributionIntoBoxes.cs
@@ -0,0 +1,28 @@
+﻿int MinimumBoxes(int[] apple, int[] capacity)
+{
+    int apples = apple.Sum();
+
+    Array.Sort(capacity);
+
+    Array.Reverse(capacity);
+
+    int index = 0;
+    while(true)
+    {
+        if (apples <=0 )
+        {
+            break;
+        }
+
+        apples -=capacity[index];
+        index++;
+        
+    }
+
+    return index;
+}
+int[] apple = [1, 3, 2], capacity = [4, 3, 1, 5, 2];
+
+int res=MinimumBoxes(apple, capacity);
+
+Console.WriteLine(res);
