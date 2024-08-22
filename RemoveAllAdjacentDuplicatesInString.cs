624e9b39596ae4cb00b67e1c763a3b2916a05582
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-23 00:13:49 +0300

    Add files via upload

---
+++ RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,31 @@
+﻿string RemoveDuplicates(string s)
+{
+    while(true)
+    {
+        bool flag = true;
+        for(int i=0; i<s.Length-1; i++)
+        {
+            if (s[i] == s[i+1])
+            {
+                s=s.Remove(i,2);
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            break;
+        }
+        
+
+       
+    }
+    return s;
+}
+
+string s = "abbaca";
+
+var res=RemoveDuplicates(s);
+
+Console.WriteLine(res);
