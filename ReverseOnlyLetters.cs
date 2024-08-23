a3c629385ceaf62dd6aa2a01d0c33df86b01752e
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-24 00:25:28 +0300

    Add files via upload

---
+++ ReverseOnlyLetters.cs
@@ -0,0 +1,36 @@
+﻿string ReverseOnlyLetters(string s)
+{
+    string temp = "";
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (char.IsLetter(s[i]))
+        {
+            temp += s[i];
+            s=s.Remove(i, 1).Insert(i,"~");
+        }
+    }
+    char[]letters=temp.ToCharArray();
+
+    Array.Reverse(letters);
+
+    string ReversedLetters=new string(letters);
+    int index = 0;
+    for(int i=0;i<s.Length;i++)
+    {
+        if(s[i]=='~')
+        {
+            s=s.Remove(i, 1).Insert(i, ReversedLetters[index].ToString());
+            index++;
+        }
+    }
+
+    return s;
+
+}
+
+string s = "a-bC-dEf-ghIj";
+
+string res=ReverseOnlyLetters(s);
+
+Console.WriteLine(res);
