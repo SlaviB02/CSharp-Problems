610c3a94d55256eefb6e2f1dc707682c3f0da203
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-26 00:51:58 +0300

    Add files via upload

---
+++ FindPeakElement.cs
@@ -0,0 +1,35 @@
+﻿int FindPeakElement(int[] nums)
+{
+    if(nums.Length==1)
+    {
+        return 0;
+    }
+    for(int i=0;i<nums.Length;i++)
+    {
+        int number=nums[i];
+
+        if(i==0)
+        {
+            if (number > nums[i+1])
+            {
+                return i;
+            }
+        }
+        if(i==nums.Length-1)
+        {
+            if(number> nums[i-1])
+            {
+                return i;
+            }
+        }
+
+        if(number> nums[i+1] && number > nums[i-1])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int[] nums = [1, 2, 3, 1];
+int res=FindPeakElement(nums);
+Console.WriteLine(res);
