3a78603b4930bab63b71291368bf1a6027e01faa
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-27 00:26:22 +0300

    Add files via upload

---
+++ PeakIndexInMountainArray.cs
@@ -0,0 +1,21 @@
+﻿int PeakIndexInMountainArray(int[] arr)
+{
+    int index = 0;
+    int temp = arr[0];
+    for(int i=1; i<arr.Length; i++)
+    {
+        if (temp < arr[i])
+        {
+            temp = arr[i];
+        }
+        else
+        {
+            index = i-1;
+            break;
+        }
+    }
+    return index;
+}
+int[] arr = [0, 1, 0];
+int res=PeakIndexInMountainArray(arr);
+Console.WriteLine(res);
