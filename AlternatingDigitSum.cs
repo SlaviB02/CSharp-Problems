9c2dc418b3bc0ba5b866b97fb431a02ecf7d3fde
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-28 09:58:13 +0300

    Add files via upload

---
+++ AlternatingDigitSum.cs
@@ -0,0 +1,30 @@
+﻿int AlternateDigitSum(int n)
+{
+    Stack<int>digits= new Stack<int>();
+
+    while(n>0)
+    {
+        int digit=n%10;
+        digits.Push(digit);
+        n/=10;
+    }
+    int index = 0;
+    int sum = 0;
+    while (digits.Count > 0)
+    {
+        int digit=digits.Pop();
+        if (index % 2 == 0)
+        {
+            sum += digit;
+        }
+        else
+        {
+            sum-= digit;
+        }
+        index++;
+    }
+    return sum;
+}
+int n = 521;
+int res=AlternateDigitSum(n);   
+Console.WriteLine(res);
