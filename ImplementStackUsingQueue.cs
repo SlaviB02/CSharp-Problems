3a8651dcc8d7b356aaeffb3aa488d8c8f0af8a22
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-30 00:06:00 +0300

    Add files via upload

---
+++ ImplementStackUsingQueue.cs
@@ -0,0 +1,38 @@
+﻿public class MyStack
+{
+    private Queue<int> list;
+    public MyStack()
+    {
+        list = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        list.Enqueue(x);
+    }
+
+    public int Pop()
+    {
+        list=new Queue<int>(list.Reverse());
+        int res=list.Dequeue();
+        list = new Queue<int>(list.Reverse());
+        return res;
+    }
+
+    public int Top()
+    {
+        list = new Queue<int>(list.Reverse());
+        int res = list.Peek();
+        list = new Queue<int>(list.Reverse());
+        return res;
+    }
+
+    public bool Empty()
+    {
+        if (list.Count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
