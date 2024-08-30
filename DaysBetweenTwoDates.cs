418b175025d7471c0fd3f3cf4f9dcb5bf42cd18a
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-08-31 00:11:43 +0300

    Add files via upload

---
+++ DaysBetweenTwoDates.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+int DaysBetweenDates(string date1, string date2)
+{
+    DateTime firstDate=DateTime.ParseExact(date1,"yyyy-MM-dd",CultureInfo.InvariantCulture);
+    DateTime secondDate = DateTime.ParseExact(date2, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+    return Math.Abs((firstDate-secondDate).Days);
+}
+string date1 = "2019-06-29", date2 = "2019-06-30";
+int res=DaysBetweenDates(date1, date2);
+Console.WriteLine(res);
