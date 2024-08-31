453a15e687836f931990f4675632ef8bee6a9c22
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-01 00:01:37 +0300

    Add files via upload

---
+++ BinaryNumberWithAlternatingBits.cs
@@ -0,0 +1,19 @@
+﻿bool HasAlternatingBits(int n)
+{
+    int OldDigit = n % 2;
+    n /= 2;
+    while(n>0)
+    {
+        int CurrentDigit=n%2;
+        if(OldDigit == CurrentDigit )
+        {
+            return false;
+        }
+        OldDigit = CurrentDigit;
+        n /= 2;
+    }
+    return true;
+}
+int n = 11;
+var res = HasAlternatingBits(n);
+Console.WriteLine(res);
