b2306cf980757e5a07e652bb819fc84471916453
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-04 00:00:37 +0300

    Add files via upload

---
+++ SumOfDigitsOfStringAfterConvert.cs
@@ -0,0 +1,24 @@
+﻿int GetLucky(string s, int k)
+{
+    string number = "";
+    for(int i=0;i<s.Length; i++)
+    {
+        number += s[i] - 96;
+    }
+    for(int i=0;i<k;i++)
+    {
+        string temp = number;
+        int sum = 0;
+        for(int j=0;j<temp.Length;j++)
+        {
+            sum += int.Parse(temp[j].ToString());
+        }
+        number = sum.ToString();
+      
+    }
+    return int.Parse(number);
+}
+string s = "leetcode";
+int k = 2;
+int res=GetLucky(s,k);
+Console.WriteLine(res);
