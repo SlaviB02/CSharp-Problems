9f22d37a2b87b0c7702dac31bd711137189ed58b
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-03 11:04:27 +0300

    Add files via upload

---
+++ StudentAttendanceRecord.cs
@@ -0,0 +1,30 @@
+﻿bool CheckRecord(string s)
+{
+    int AbsentDays = 0;
+    int LateDays = 0;
+    for(int i= 0; i < s.Length; i++)
+    {
+        if(s[i] =='A')
+        {
+            AbsentDays++;
+            LateDays = 0;
+        }
+        else if (s[i]=='L')
+        {
+            LateDays++;
+        }
+        else
+        {
+            LateDays = 0;
+        }
+        if(LateDays>=3)
+        {
+            return false;
+        }
+    }
+    if(AbsentDays<2)
+    {
+        return true;
+    }
+    return false;
+}
