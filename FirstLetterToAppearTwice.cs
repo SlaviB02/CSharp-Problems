3a1e93adb855d16b874a065f205a810eb46410b1
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-06 09:44:27 +0300

    Add files via upload

---
+++ FirstLetterToAppearTwice.cs
@@ -0,0 +1,20 @@
+﻿char RepeatedCharacter(string s)
+{
+    List<char>letters=new List<char>();
+
+    for(int i=0; i<s.Length; i++)
+    {
+        if (!letters.Contains(s[i]))
+        {
+            letters.Add(s[i]);
+        }
+        else
+        {
+            return s[i];
+        }
+    }
+    return '-';
+}
+string s = "abccbaacz";
+char res=RepeatedCharacter(s);
+Console.WriteLine(res);
