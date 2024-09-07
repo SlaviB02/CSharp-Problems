b2913e9351317c9661b8d51341df6254a845b691
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-07 11:27:57 +0300

    Add files via upload

---
+++ CountNumberOfConsistentStrings.cs
@@ -0,0 +1,28 @@
+﻿int CountConsistentStrings(string allowed, string[] words)
+{
+   List<char>allowedChars=allowed.ToList();
+    int count = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        bool flag = true;
+        for(int j = 0; j < words[i].Length;j++)
+        {
+            if (!allowedChars.Contains(words[i][j]))
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            count++;
+        }
+    }
+
+    return count;
+
+}
+string allowed = "ab";
+string[] words = ["ad", "bd", "aaab", "baa", "badab"];
+int res=CountConsistentStrings(allowed, words);
+Console.WriteLine(res);
