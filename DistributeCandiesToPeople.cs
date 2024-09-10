524ac84eea4365874daece2db87d66bf548d48df
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-11 00:02:21 +0300

    Add files via upload

---
+++ DistributeCandiesToPeople.cs
@@ -0,0 +1,36 @@
+﻿int[] DistributeCandies(int candies, int num_people)
+{
+    int[]res=new int[num_people];
+    int index = 0;
+    int candyNumber = 1;
+    while(true)
+    {
+        if(index==num_people)
+        {
+            index = 0;
+        }
+        if(candies<=0)
+        {
+            break;
+        }
+
+        if (candies >= candyNumber)
+        {
+            res[index] += candyNumber;
+            candies -= candyNumber;
+            candyNumber++;
+            
+        }
+        else
+        {
+            res[index]+=candies;
+            candies = 0;
+        }
+        index++;
+
+    }
+    return res;
+}
+int candies = 7, num_people = 4;
+var res=DistributeCandies(candies, num_people);
+Console.WriteLine(string.Join(",", res));
