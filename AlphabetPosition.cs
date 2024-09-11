db05ebe66c7eb0416b878e19bcd8fd789b1b204b
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-12 00:09:56 +0300

    Add files via upload

---
+++ AlphabetPosition.cs
@@ -0,0 +1,24 @@
+﻿string AlphabetPosition(string text)
+{
+    string res = "";
+    for(int i=0;i<text.Length;i++)
+    {
+        char letter=text[i];
+        if(char.IsLetter(letter))
+        {
+            if (char.IsUpper(letter))
+            {
+                res += letter - 64 + " ";
+            }
+            else
+            {
+                res += letter - 96 + " ";
+            }
+           
+        }
+    }
+    return res.TrimEnd();
+}
+string text = "The sunset sets at twelve o' clock.";
+string res=AlphabetPosition(text);
+Console.WriteLine(res);
