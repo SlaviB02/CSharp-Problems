4beb6de591cb8ab7f89a4941add3e4b55ba31820
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-14 00:01:46 +0300

    Add files via upload

---
+++ XORQueriesOfSubarray.cs
@@ -0,0 +1,21 @@
+﻿int[] XorQueries(int[] arr, int[][] queries)
+{
+    int[]res= new int[queries.Length];
+    for(int i=0;i<queries.Length;i++)
+    {
+        int start=queries[i][0];
+        int end=queries[i][1];
+        int xor = arr[start];
+            for (int j = start + 1; j <= end; j++)
+            {
+                xor ^= arr[j];
+            }
+ 
+        res[i]=xor;
+    }
+    return res;
+}
+int[] arr = [1, 3, 4, 8];
+int[][] queries = [[0, 1], [1, 2], [0, 3], [3, 3]];
+int[]res=XorQueries(arr, queries);
+Console.WriteLine(string.Join(",", res));
