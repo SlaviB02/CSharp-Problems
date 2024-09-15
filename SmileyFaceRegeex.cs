b2f3ef7e9633a56f449f684e318adccc9908e5b3
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-15 11:27:40 +0300

    Add files via upload

---
+++ SmileyFaceRegeex.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+int CountSmileys(string[] smileys)
+{
+    string input = string.Join(" ",smileys);
+   
+    string regex = "(:|;){1}(-|~)*(\\)|D){1}";
+    var matches = Regex.Matches(input, regex);
+
+    return matches.Count;
+}
+string[] input = [":)", ";(", ";}", ":-D"];
+int res=CountSmileys(input);
+Console.WriteLine(res);
