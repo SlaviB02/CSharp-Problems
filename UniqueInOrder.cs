96405e9c3cd80b442ed2c1f8c362ea47abb7d900
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-16 00:04:53 +0300

    Add files via upload

---
+++ UniqueInOrder.cs
@@ -0,0 +1,24 @@
+﻿IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+{
+    List<T>res=new List<T>();
+    int index = 0;
+    foreach (T item in iterable)
+    {
+        if(index==0)
+        {
+            res.Add(item);
+            index++;
+            
+        }
+        else if (!item.Equals(res[index-1]))
+        {
+            res.Add(item);
+            index++;
+        }
+        
+    }
+    return res;
+ }
+string a = "AAAABBBCCDAABBB";
+var res=UniqueInOrder(a);
+Console.WriteLine(string.Join(",",res));
