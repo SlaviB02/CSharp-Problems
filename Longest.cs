6d423232f135670d7f16f6a586e315b76e67b7ce
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-17 09:29:38 +0300

    Add files via upload

---
+++ Longest.cs
@@ -0,0 +1,26 @@
+﻿string Longest(string s1, string s2)
+{
+    char letter = 'a';
+    string res = "";
+    while(true)
+    {
+        if (s1.Contains(letter))
+        {
+            res += letter;
+        }
+        else if (s2.Contains(letter))
+        {
+            res += letter;
+        }
+        else if(letter=='z')
+        {
+            break;
+        }
+        letter++;
+        
+    }
+    return res;
+}
+string a = "xyaabbbccccdefww",b = "xxxxyyyyabklmopq";
+var res=Longest(a,b);
+Console.WriteLine(res);
