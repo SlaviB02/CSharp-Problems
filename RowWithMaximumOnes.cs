2f9f7ea7da94ab8c05872cdba60a8cc17008043c
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-18 10:36:58 +0300

    Add files via upload

---
+++ RowWithMaximumOnes.cs
@@ -0,0 +1,28 @@
+﻿int[] RowAndMaximumOnes(int[][] mat)
+{
+    int[]res= new int[2];
+    int max = 0;
+    int index = 0;
+    for(int i=0; i<mat.Length; i++)
+    {
+        int ZeroCount = 0;
+        for(int j=0; j<mat[i].Length; j++)
+        {
+            if (mat[i][j]==1)
+            {
+                ZeroCount++;
+            }
+        }
+        if(max<ZeroCount)
+        {
+            max=ZeroCount;
+            index = i;
+        }
+    }
+    res[0] = index;
+    res[1] = max;
+    return res;
+}
+int[][] mat = [[0, 0, 0], [0, 1, 1]];
+int[]res=RowAndMaximumOnes(mat);
+Console.WriteLine(res[0] + " " + res[1]);
