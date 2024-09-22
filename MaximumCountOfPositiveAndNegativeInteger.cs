7ce34ea99f812955dea96da96657d24d2061f8a2
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-23 01:08:03 +0300

    Add files via upload

---
+++ MaximumCountOfPositiveAndNegativeInteger.cs
@@ -0,0 +1,20 @@
+﻿int MaximumCount(int[] nums)
+{
+    int PCount = 0;
+    int NCount = 0;
+    for(int i=0; i<nums.Length; i++)
+    {
+        if (nums[i] < 0)
+        {
+            NCount++;
+        }
+        else
+        {
+            PCount++;
+        }
+    }
+    return Math.Max(PCount, NCount);
+}
+int[] nums = [-2, -1, -1, 1, 2, 3];
+int res=MaximumCount(nums);
+Console.WriteLine(res);
