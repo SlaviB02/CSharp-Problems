ddb5e44c2b66f2f59cc57d3f47d68e3407023927
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-24 12:04:11 +0300

    Add files via upload

---
+++ NumberOfUnequalTripletsInArray.cs
@@ -0,0 +1,21 @@
+﻿int UnequalTriplets(int[] nums)
+{
+    int count = 0;
+    for(int i=0; i<nums.Length; i++)
+    {
+        for(int j=i+1; j<nums.Length; j++)
+        {
+            for(int z=j+1; z<nums.Length; z++)
+            {
+                if (nums[i]!=nums[j] && nums[i] != nums[z] && nums[j] != nums[z])
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+int[] nums = [4, 4, 2, 4, 3];
+int res=UnequalTriplets(nums);
+Console.WriteLine(res);
