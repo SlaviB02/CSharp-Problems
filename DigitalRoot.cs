1766f2466c8071d0c3b6764425043a745441b8ca
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-09-25 19:09:25 +0300

    Add files via upload

---
+++ DigitalRoot.cs
@@ -0,0 +1,21 @@
+﻿static int DigitalRoot(long n)
+{
+    long current = 0;
+    while (true)
+    {
+        current = 0;
+        while (n > 0)
+        {
+            current += n % 10;
+            n /= 10;
+        }
+        if (current < 10)
+        {
+            return int.Parse(current.ToString());
+        }
+        n = current;
+    }
+}
+long n = 493193;
+int res=DigitalRoot(n);
+Console.WriteLine(res);
